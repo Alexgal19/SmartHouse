4f473759622e4a0826ddf6652cac2da99e5a777e
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-10 22:00:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
 };
 
 export default nextConfig;
+
+    
