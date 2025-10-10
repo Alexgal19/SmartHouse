125c26bf4192305ec7755ad2537cdd05537c790a
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-10 20:04:09 +0000

    ось доступ до Google sheethttps://docs.google.com/spreadsheets/d/1UYe8N2

--- src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import MainLayout from '@/components/main-layout';
-
-export default function DashboardPage() {
-  return <MainLayout />;
-}
-
-    
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -904,5 +904,3 @@ export async function checkAndUpdateEmployeeStatuses(actor: Coordinator): Promis
     throw new Error("Could not update statuses.");
   }
 }
-
-    
