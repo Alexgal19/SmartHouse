8051e8d2e40f202338c5824ffd2fed7eb3919e8f
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-10 21:56:53 +0000

    Try fixing this error: `Runtime Error: Error: GOOGLE_SERVICE_ACCOUNT_EMA

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Environment variables are available on the server-side by default,
-  // this configuration is not needed to expose them to server components/actions.
-  // The 'env' key is for exposing variables to the browser, which is not what's needed here.
 };
 
 export default nextConfig;
