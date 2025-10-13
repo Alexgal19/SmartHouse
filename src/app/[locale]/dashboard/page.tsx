26b90081b55121cea82d6547fccb7c36b6b66f28
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-13 20:17:51 +0000

    програма постійно перезавантажується

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -138,7 +138,8 @@ function DashboardPageContent() {
                 handleRefreshStatuses(false);
             });
         }
-    }, [currentUser, fetchAllData, handleRefreshStatuses]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentUser]);
 
     
     useEffect(() => {
@@ -462,3 +463,5 @@ export default function DashboardPage() {
         </Suspense>
     )
 }
+
+    
