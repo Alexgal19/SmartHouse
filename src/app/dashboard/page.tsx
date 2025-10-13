5dac162bbb0d7807432165e1fc132f7763239522
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-13 20:49:37 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 
+"use client"
 import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function DashboardRootPage() {
-  redirect('/pl/dashboard');
-}
+  useEffect(() => {
+    redirect('/pl/dashboard');
+  }, []);
 
+  return null;
+}
