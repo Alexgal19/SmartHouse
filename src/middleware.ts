6f94a82d882889a2d516d662fbd01b846cb358e7
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-13 20:00:31 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
+import { locales } from './navigation';
  
-const locales = ['pl', 'en', 'uk', 'es'];
-
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: locales,
- 
-  // Used when no locale matches
-  defaultLocale: 'pl'
+  defaultLocale: 'pl',
+  locales,
 });
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    // Match all pathnames within the locales defined above
-    '/(pl|en|uk|es)/:path*'
-  ]
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
