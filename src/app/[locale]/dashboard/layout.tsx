ab8f8f2f28241557d5bf53fd9950c20555927c0b
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-13 21:12:00 +0000

    ?

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import { Inter } from 'next/font/google';
+import '../globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { Toaster } from '@/components/ui/toaster';
+import { ReactNode } from 'react';
 
-export default function DashboardLayout({
+const inter = Inter({ subsets: ['latin'] });
+
+interface Props {
+  children: ReactNode;
+  params: {
+    locale: string;
+  };
+}
+
+export default async function RootLayout({ 
   children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
+  params: {locale}
+}: Props) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+          <Toaster />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
