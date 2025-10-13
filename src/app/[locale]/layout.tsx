63f2004b98fc50a5034730e3e9c838b143d91a40
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-13 19:01:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,19 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ 
+export default async function RootLayout({ 
   children,
   params: {locale}
 }: { 
   children: React.ReactNode;
   params: {locale: string};
 }) {
-  const messages = useMessages();
+  const messages = await getMessages({locale});
 
   return (
     <html lang={locale}>
