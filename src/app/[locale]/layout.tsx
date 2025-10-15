5194bbde2f59d12d20359822d666abf2545e3367
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-15 18:03:49 +0000

    Що це?
    arning: Extra attributes from the server: class
        at body

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,33 +1,11 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import { Toaster } from '@/components/ui/toaster';
 import { ReactNode } from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
-
-interface Props {
+// Since the root layout already handles language and providers,
+// this layout can be very simple.
+export default function LocaleLayout({ 
+  children
+}: { 
   children: ReactNode;
-  params: {
-    locale: string;
-  };
-}
-
-export default async function RootLayout({ 
-  children,
-  params: {locale}
-}: Props) {
-  const messages = await getMessages();
-
-  return (
-    <html lang={locale}>
-      <body className={inter.className}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-          <Toaster />
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+}) {
+  return children;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,34 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { Toaster } from '@/components/ui/toaster';
 import { ReactNode } from 'react';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+const inter = Inter({ subsets: ['latin'] });
+
+interface Props {
+  children: ReactNode;
+  params: {
+    locale: string;
+  };
+}
+
+export default async function RootLayout({ 
+  children,
+  params: {locale}
+}: Props) {
+  // Providing all messages to the client
+  // side is a good default
+  const messages = await getMessages();
+
   return (
-    <html>
-      <body>
-        {children}
+    <html lang={locale}>
+      <body className={inter.className}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+          <Toaster />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
