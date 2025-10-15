b861c58f086ff3e50d8238e31e62e1e76d94a903
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-15 18:23:11 +0000

    /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
 import { Toaster } from '@/components/ui/toaster';
 import { ReactNode } from 'react';
+import { locales } from '@/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,11 +15,16 @@ interface Props {
   };
 }
 
-export default function RootLayout({ 
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({ 
   children,
   params: {locale}
 }: Props) {
-  const messages = useMessages();
+  unstable_setRequestLocale(locale);
+  const messages = await getMessages();
 
   return (
     <html lang={locale}>
