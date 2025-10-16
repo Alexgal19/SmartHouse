5b3505103f6e4c90cf365494d530fb76b6f41754
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-16 14:44:45 +0000

    Type 'typeof import("/home/user/studio/src/app/[locale]/dashboard/layout

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import MainLayout from '@/components/main-layout';
 import { ReactNode } from 'react';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
     children,
     params: {locale}
   }: {
