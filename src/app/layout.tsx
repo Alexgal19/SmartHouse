7ae1701136766b049e7b54dbb95bfb323ce5a193
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-16 18:03:43 +0000

    так виправ ось цю помилку на фото.програма не запускається

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pl">
+    <html lang="en">
       <body className={inter.className}>
           {children}
           <Toaster />
