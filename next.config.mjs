e086998f48a0c78d65aae66f47d1dc7bb91a2446
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-16 17:48:58 +0000

    Починай

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
-    // This is required for Firebase Studio to work correctly.
-    allowedDevOrigins: [
-      '6000-firebase-studio-1759406863592.cluster-55m56i2mgjalcvl276gecmncu6.cloudworkstations.dev',
-    ],
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
   },
 };
 
