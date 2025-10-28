06b209aab7d544ed4607562b80484cb3a904aedc
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-28 06:34:43 +0000

    Виправ Проблему, в мене файл ексель займає 10 мегабайт

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '15mb',
+        },
+    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
