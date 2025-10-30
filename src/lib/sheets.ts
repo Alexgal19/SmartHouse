2d692969d5dcf4d2c3b84b2225ac276da7acd622
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-30 14:09:23 +0000

    всеодно кнопка Dodaj не реагує .нічого не відбувається.не зберігає впров

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -434,5 +434,3 @@ export const getAllSheetsData = async () => {
         throw new Error(`Could not fetch all data from sheets. Original error: ${error instanceof Error ? error.message : "Unknown error"}`);
     }
 }
-
-    
