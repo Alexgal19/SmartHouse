5000b2b6a88c5e84b2beb5f5db4099721510d5f8
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-10-31 12:13:58 +0000

    в мобільній версії натискаючи на посилання на програму відкривається пус

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,13 @@
-// Basic service worker for PWA installation
-
-const CACHE_NAME = 'smarthouse-cache-v1';
+const CACHE_NAME = 'smarthouse-cache-v2';
 const urlsToCache = [
   '/',
   '/login',
-  '/dashboard?view=dashboard',
   '/manifest.json',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'
 ];
 
+// Install a service worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -20,15 +18,41 @@ self.addEventListener('install', event => {
   );
 });
 
+// Cache and return requests
 self.addEventListener('fetch', event => {
+  // We are implementing a Network falling back to cache strategy
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).then(res => {
+        // We got a response from the network.
+        const resClone = res.clone();
+        // Open the cache and store the latest version of the file.
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+                cache.put(event.request, resClone);
+            });
+        return res;
+    }).catch(err => {
+        // The network request failed.
+        // Try to get the file from the cache.
+        return caches.match(event.request).then(res => res);
+    })
+  );
+});
+
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
